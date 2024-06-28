23c44ea1428769579224ce137cc6cebd7c6c3400
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2024-06-28 22:27:46 +0100

    Initial commit - Django project and app

---
+++ bookkeeping_app/transactions/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Transaction(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    transaction_type = models.CharField(max_length=50) # e.g., income or expense
+    
